b0b3177f755ff76a28442ecb7a331c2ebad6e06e
Tiago França <tiago@tiagofranca.com>
2024-09-18 22:46:44 -0300

    embeded view
    On branch task/file-manager-ui
    	modified:   resources/js/Pages/Files/Embedded.vue

--- resources/js/Pages/Files/Embedded.vue
+++ resources/js/Pages/Files/Embedded.vue
@@ -17,7 +17,6 @@ const props = defineProps({
 
 <template>
 <iframe
-    v-if="false"
     width="100%"
     height="100%"
     style="width: 100%; height: 100vh;"
@@ -27,5 +26,4 @@ const props = defineProps({
     referrerpolicy="no-referrer-when-cross-origin"
     allowfullscreen
 ></iframe>
-<iframe width="100%" height="100%" style="width: 100%; height: 100vh;" src="https://files.tiagofranca.com/files/render/pPy7LDdwRb1YKXA" title="Pro Library" referrerpolicy="no-referrer-when-cross-origin" frameborder="0" allowfullscreen ></iframe>
 </template>
