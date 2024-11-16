0f51423b69bd5a0a3dd44aaac5b2b3b9fd69b9e8
Tiago França <tiago@tiagofranca.com>
2024-11-16 10:09:25 -0300

    allow theme change on login page
    On branch task/legacy-browsers-fix
    	modified:   resources/js/Components/ApplicationLogo.vue
    	modified:   resources/js/Layouts/GuestLayout.vue

--- resources/js/Layouts/GuestLayout.vue
+++ resources/js/Layouts/GuestLayout.vue
@@ -1,14 +1,21 @@
 <script setup>
 import ApplicationLogo from '@/Components/ApplicationLogo.vue';
 import { Link } from '@inertiajs/vue3';
+import { initFlowbite } from 'flowbite';
+import { onMounted } from 'vue';
+import ToggleDarkMode from '@/Layouts/parts/ToggleDarkMode.vue';
+onMounted(() => {
+    initFlowbite();
+})
 </script>
 
 <template>
     <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
-        <div>
-            <Link href="/">
-                <ApplicationLogo class="w-20 h-20 fill-current text-gray-500" />
+        <div class="flex md:flex-col w-full md:w-3/12 justify-content-center items-center gap-y-3">
+            <Link href="/"  class="flex w-full justify-content-center items-center">
+                <ApplicationLogo class="w-20 h-20 mx-auto fill-current text-gray-500" />
             </Link>
+            <ToggleDarkMode/>
         </div>
 
         <div
