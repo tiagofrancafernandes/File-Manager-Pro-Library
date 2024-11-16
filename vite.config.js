d9520dbbf84a1301b70e1a2e821efcef74eb2ae5
Tiago França <tiago@tiagofranca.com>
2024-11-16 10:08:44 -0300

    try to fix on legacy browsers
    On branch task/legacy-browsers-fix
    	modified:   package-lock.json
    	modified:   package.json
    	modified:   vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig, loadEnv } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
+import legacy from '@vitejs/plugin-legacy';
 
 export default defineConfig(({ command, mode }) => {
     const env = loadEnv(mode, process.cwd(), '');
@@ -13,6 +14,9 @@ export default defineConfig(({ command, mode }) => {
             __APP_ENV__: `"${env.APP_ENV}"`,
         },
         plugins: [
+            legacy({
+                targets: ['defaults', 'not IE 11'],
+            }),
             laravel({
                 input: [
                     'resources/js/app.js',
@@ -39,6 +43,9 @@ export default defineConfig(({ command, mode }) => {
                 '@asset': fileURLToPath(new URL('./public', import.meta.url)),
                 '@base': fileURLToPath(new URL('./', import.meta.url)),
             }
+        },
+        build: {
+            chunkSizeWarningLimit: '2M'
         }
     }
 });
